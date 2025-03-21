d4d2e1b2eda1eefeae5a4ebfcf1e1b17933f511d
Adam Gastineau <adam@agg.im>
2025-03-21 08:22:13 -0700

    More spy testing

--- tests/specs/spy.spec.tsx
+++ tests/specs/spy.spec.tsx
@@ -1,9 +1,16 @@
 import { test, expect } from "pw-ct";
 import * as React from "react";
 import { stripVTControlCharacters } from "util";
+import importCheck from "./importCheck.ts";
 
-test('should record basic call on spy', async ({ page, mount, $browserSpy }) => {
-  const aFunctionSpy = await $browserSpy(() => console.log("hello world. This is in browser"));
+test('should record basic call on spy', async ({ page, mount, $browserSpy, $browser }) => {
+  const didImport = await $browser(false);
+  
+  const aFunctionSpy = await $browserSpy(() => {
+    didImport = !!importCheck;
+    console.log(importCheck ? "Wrapped imports" : "Not wrapped imports");
+    console.log("hello world. This is in browser");
+  });
 
   await mount.react(() => {
     return <button onClick={aFunctionSpy}>Hi</button>;
@@ -15,6 +22,9 @@ test('should record basic call on spy', async ({ page, mount, $browserSpy }) => 
 
   await expect(aFunctionSpy).toHaveBeenCalled();
   await expect(aFunctionSpy).toHaveBeenCalledTimes(1);
+
+  // TODO: Check for spy dependencies
+  expect(await didImport.get()).toBe(true);
 });
 
 test('should properly error when spy not called with toHaveBeenCalled', async ({ page, mount, $browserSpy }) => {
@@ -51,4 +61,40 @@ test(`should record each call's arguments`, async ({ page, mount, $browserSpy })
   await expect(aFunctionSpy).toHaveBeenCalledWith("hello", 0);
   await expect(aFunctionSpy).toHaveBeenCalledWith("hello", 1);
   await expect(aFunctionSpy).toHaveBeenCalledWith("hello", 2);
+});
+
+test('should allow bidirectional communication', async ({ page, mount, $browserSpy, $browser }) => {
+  let exposedCalled = false;
+  await page.exposeBinding('getUUID', (_, name: string) => {
+    console.log(name);
+    exposedCalled = true;
+    return crypto.randomUUID() + name;
+  });
+
+  const receivedBrowserUUID = await $browser<string | undefined>(undefined);
+
+  await mount.react(() => {
+    const getUUID = async () => {
+      const startUUID = receivedBrowserUUID;
+      const uuid = await window['getUUID']("hello") + startUUID;
+      console.log(uuid);
+      receivedBrowserUUID = uuid;
+    };
+
+    return <button onClick={getUUID}>Get UUID</button>;
+  });
+
+  await page.locator('button').click();
+
+  expect(exposedCalled).toBeTruthy();
+  expect(await receivedBrowserUUID.get()).toContain("hello");
+
+  await receivedBrowserUUID.set("new value");
+
+  await page.locator('button').click();
+
+  expect(await receivedBrowserUUID.get()).toContain("hello");
+  expect(await receivedBrowserUUID.get()).toContain("new value");
+
+  // TODO: Add sending an event to the page
 });
